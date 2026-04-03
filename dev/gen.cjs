409ffb2c9af7b1b7b796c98a05dd3d0679f206af
frto027 <q602706150@gmail.com>
2026-04-03 11:02:55 +0800

    clean code

--- dev/gen.cjs
+++ dev/gen.cjs
@@ -1,15 +1,3 @@
-//         这是一个神奇的编译脚本
-//         它读取CraftingUI.original.js的源码，然后做这些事情：
-
-//         它会将下面的字符串file path替换为路径为file path的文件内容
-//         /*@read-file*/"file path"
-
-//         如果不加--debug参数执行，它会
-//         删除@debug-only注释后的语句
-//         删除所有包含@remove-comment的注释
-
-//         最后，它输出CraftingUI.compiled.release.js，此源代码已经转换，符合IE8的语法规范
-
 const babel = require("@babel/core")
 const fs = require("fs")
 
